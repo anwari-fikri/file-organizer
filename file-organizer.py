2904a54f7ac2b40617f7b76989f990eef2146483
anwari-fikri <anwari.fikri@gmail.com>
2023-07-25 09:41:30 +0800

    first commit

---
+++ file-organizer.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+
+path = "C:\\Users\\AK MD ANWARI FIKRI\\Downloads\\"
+filename = os.listdir(path)
+
+folders = {
+    "images": [".jpg", ".png", ".gif"],
+    "exe": [".exe", ".msi"],
+    "videos": [".mp4", ".avi", ".mov"],
+    "office": [".docx", ".xlsx", ".pptx", ".pdf"]
+}
+
+for folder in folders:
+    if not os.path.exists(os.path.join(path, folder)):
+        os.makedirs(os.path.join(path, folder))
+
+for file in filename:
+    for folder, extensions in folders.items():
+        for ext in extensions:
+            if ext in file and not os.path.exists(os.path.join(path, folder, file)):
+                shutil.move(os.path.join(path, file), os.path.join(path, folder, file))
+                break
